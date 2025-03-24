954abeae8595a766c0b4ea068eda3bc880b7a5cc
Adarsh mahant <168413166+Adarshmahant08@users.noreply.github.com>
2025-03-24 10:45:34 +0530

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    // this is basic output syntax
+    printf("adarsh prashant \"mahant\" \n");
+    printf("adarsh\t mahant");
+    return 0;
+
+    // C variables
+    
+}
